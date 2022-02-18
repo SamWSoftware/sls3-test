c1387cf310edcf7c1a87aab87969797415e08aa4
Sam Williams <sam@completecoding.io>
2022-02-18 17:15:38 +0100

    init

---
+++ serverless.ts
@@ -0,0 +1,72 @@
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const serverlessConfiguration: AWS = {
+  service: 'sls-v3-test',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    region: 'eu-central-1',
+    
+    environment: {
+      someVariable: '${env:someVariable}',
+    },
+  },
+  useDotenv: true,
+
+  params: {
+    dev: {
+      message: 'Woop Woop, dev has been deployed.',
+      dynamoTableName: 'dev-sls-v3-test',
+    },
+    prod: {
+      message: 'Now the stage is prod.',
+      dynamoTableName: 'dev-sls-v3-test',
+    }
+  },
+  functions: { hello },
+  resources: {
+    Resources: {
+      table1: {
+          Type: 'AWS::DynamoDB::Table',
+          DeletionPolicy: 'Retain',
+          Properties: {
+              TableName: '${param.tableName}',
+              AttributeDefinitions: [
+                  {
+                      AttributeName: 'id',
+                      AttributeType: 'S',
+                  },
+              ],
+  
+              KeySchema: [
+                  {
+                      AttributeName: 'id',
+                      KeyType: 'HASH',
+                  },
+              ],
+              BillingMode: 'PAY_PER_REQUEST',
+          },
+      },
+    }
+  },
+
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
